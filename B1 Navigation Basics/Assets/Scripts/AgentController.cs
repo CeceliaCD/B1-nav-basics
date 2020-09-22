a9bb758233d9dda2c396804a622abf6e541c7d4a
ZachLondono <zachary.londono5@gmail.com>
2020-09-22 14:11:09 -0400

    Created Project
    Created project and basic file structure

---
+++ B1 Navigation Basics/Assets/Scripts/AgentController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour {
+
+    private bool selected;
+
+    void OnMouseOver() {
+        Debug.Log(selected);
+        if (Input.GetMouseButtonDown(0)) {
+            selected = !selected;
+        }
+    }
+
+}
---
+++ B1 Navigation Basics/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    void Update() {
+
+        float moveVertical = Input.GetAxis("Vertical") / 4.0f;        
+        float moveHorizontal = Input.GetAxis("Horizontal") / 4.0f;                
+        float moveDepth = Input.GetAxis("Depth") / 4.0f;
+
+        transform.position = new Vector3(transform.position.x + moveHorizontal, transform.position.y + moveDepth, transform.position.z + moveVertical);
+
+
+    }
+}
