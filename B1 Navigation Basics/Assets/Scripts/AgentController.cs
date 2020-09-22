bd6c7aa654fe80aa52f4a19ad44c8cd66b3ffadc
ZachLondono <zachary.londono5@gmail.com>
2020-09-22 15:01:22 -0400

    Agents will change color depending on if they are selected

--- B1 Navigation Basics/Assets/Scripts/AgentController.cs
+++ B1 Navigation Basics/Assets/Scripts/AgentController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class AgentController : MonoBehaviour {
 
     private bool selected;
+    private Material material;
+
+    void Start() {
+        material = gameObject.GetComponent<Renderer>().material;
+    }
 
     void OnMouseOver() {
-        Debug.Log(selected);
         if (Input.GetMouseButtonDown(0)) {
             selected = !selected;
+            if (selected) material.SetColor("_Color", Color.green);
+            else material.SetColor("_Color", Color.red);
         }
     }
 
