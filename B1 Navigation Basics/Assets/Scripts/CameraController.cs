8b28a17a06767a3584b78e0aea94ca8995d9aed0
ZachLondono <zachary.londono5@gmail.com>
2020-09-23 17:33:35 -0400

    Added camera free look, when the 'look key' is held down

--- B1 Navigation Basics/Assets/Scripts/CameraController.cs
+++ B1 Navigation Basics/Assets/Scripts/CameraController.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject target;
+    public float movementMultiplier;
+    public float lookMultiplierX, lookMultiplierY;
+    public string lookButton;
 
     void Update() {
 
-        float moveVertical = Input.GetAxis("Vertical") / 4.0f;        
-        float moveHorizontal = Input.GetAxis("Horizontal") / 4.0f;                
-        float moveDepth = Input.GetAxis("Depth") / 4.0f;
+        float moveVertical = Input.GetAxis("Vertical") * movementMultiplier;        
+        float moveHorizontal = Input.GetAxis("Horizontal") * movementMultiplier;                
+        float moveDepth = Input.GetAxis("Depth") * movementMultiplier;
 
         transform.position = transform.position + transform.forward * moveVertical + transform.right * moveHorizontal + transform.up * moveDepth;
         
+        if (Input.GetKey(lookButton)) {
+            Debug.Log("Look Toggled");
+            transform.eulerAngles = transform.eulerAngles + new Vector3(-1 * lookMultiplierY * Input.GetAxis("Mouse Y"), lookMultiplierX * Input.GetAxis("Mouse X"), 0.0f);
+        }
+
         Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
         if(Physics.Raycast(myRay, out hitInfo)) {
