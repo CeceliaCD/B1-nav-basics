47c1f24af2bc1177a37784fcd53e4e6e8730a4c6
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2020-09-25 02:52:58 -0400

    Added cubeObstacleMotion script and script for obstacle with fixed pathways but did not finish latter yet. Also gave the cubes rigid body components.

---
+++ B1 Navigation Basics/Assets/Scripts/ObstacleMotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ObstacleMotion : MonoBehaviour
+{
+    NavMeshObstacle cubeObstacle;
+    private float speed = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       cubeObstacle = GetComponent<NavMeshObstacle>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveDepth = Input.GetAxis("Depth");
+        
+        transform.position = transform.position + new Vector3(moveHorizontal*speed, 0, moveDepth*speed);
+    }
+}
